ff13bc3cfcd38d2d185a4f6d13d41bbb11326bd8
VinylKnight <33336283+VinylKnight@users.noreply.github.com>
2020-03-06 22:34:08 -0600

    Interactive Objects (SFX)

--- Assets/Scripts/InteractiveObject.cs
+++ Assets/Scripts/InteractiveObject.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof (AudioSource))]
 public class InteractiveObject : MonoBehaviour, IInteractive
 {
+    [SerializeField]
+    private AudioSource audioSource;
     [SerializeField]
     private string displayText = nameof(InteractiveObject);
 
     public string DisplayText => displayText;
 
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void interactWith()
     {
+        try
+        {
+         audioSource.Play();
+        }
+        catch(System.Exception)
+        {
+         throw new System.Exception("Missing AudioSource: Interactive objects require an AudioSource component.");
+        }
         Debug.Log($"Player just interacted with {gameObject.name}.");
     }
 }
