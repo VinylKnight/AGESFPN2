f29eb58e8e41c8c79353061efa46f9413be2f5fe
VinylKnight <33336283+VinylKnight@users.noreply.github.com>
2020-04-01 21:52:22 -0500

    Doors Part 2
    1:08:55

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Door : InteractiveObject
+{
+    private Animator animator;
+
+    public Door()
+    {
+
+    }
+
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+}
