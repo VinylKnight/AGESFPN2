1c04511ad2b4ced0d960d463d8331aa51775a113
VinylKnight <33336283+VinylKnight@users.noreply.github.com>
2020-04-27 11:44:07 -0500

    Inventory Menu Functionality (Part 2)

--- Assets/Scripts/InventoryMenu.cs
+++ Assets/Scripts/InventoryMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class InventoryMenu : MonoBehaviour
 {
     private static InventoryMenu instance;
-
+    private CanvasGroup canvasGroup;
     public static InventoryMenu Instance
     {
         get
@@ -18,11 +18,44 @@ public class InventoryMenu : MonoBehaviour
         private set { instance = value; }
     }
 
+    private bool IsVisible => canvasGroup.alpha > 0;
+
+    private void ShowMenu()
+    {
+        canvasGroup.alpha = 1;
+        canvasGroup.interactable = true;
+    }
+
+    private void HideMenu()
+    {
+        canvasGroup.alpha = 0;
+        canvasGroup.interactable = false;
+    }
+
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetButtonDown("Show Inventory Menu"))
+        {
+            if (IsVisible)
+                HideMenu();
+            else
+                ShowMenu();
+        }
+    }
+
     private void Awake()
     {
         if (instance == null)
             instance = this;
         else
             throw new System.Exception("There is already an instance of InventoryMenu and there can only be one.");
+
+        canvasGroup = GetComponent<CanvasGroup>();
+        HideMenu();
     }
 }
