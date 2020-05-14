85bec2ae26186537d42ed2164717abc9443b60a9
VinylKnight <33336283+VinylKnight@users.noreply.github.com>
2020-05-14 00:06:11 -0500

    Addition Of Credits

--- Assets/Scripts/OpenLink.cs
+++ Assets/Scripts/OpenLink.cs
@@ -4,24 +4,68 @@ using UnityEngine;
 
 public class OpenLink : MonoBehaviour
 {
-    public void OpenSound()
+    public void OpenChestFullSound()
     {
-        Application.OpenURL("https://freesound.org/");
+        Application.OpenURL("https://freesound.org/people/ssierra1202/sounds/391950/");
     }
-
+    public void OpenBadBeepSound()
+    {
+        Application.OpenURL("https://freesound.org/people/RICHERlandTV/sounds/216090/");
+    }
+    public void OpenKeyboardSound()
+    {
+        Application.OpenURL("https://freesound.org/people/xtrgamr/sounds/252715/");
+    }
+    public void OpenBagSound()
+    {
+        Application.OpenURL("https://freesound.org/people/detamine/sounds/458372/");
+    }
+    public void OpenLockedDoorSound()
+    {
+        Application.OpenURL("https://freesound.org/people/Leady/sounds/12739/");
+    }
+    public void OpenGateLatchSound()
+    {
+        Application.OpenURL("https://freesound.org/people/mhtaylor67/sounds/126041/");
+    }
     public void OpenSnapsAsset()
     {
         Application.OpenURL("https://assetstore.unity.com/packages/3d/environments/snaps-prototype-office-137490");
     }
-
-    public void OpenMusic()
+    public void OpenCoinsAsset()
     {
-        Application.OpenURL("https://incompetech.com/music/");
+        Application.OpenURL("https://assetstore.unity.com/packages/3d/props/gold-coins-1810");
     }
-
-    public void OpenPurplePlanet()
+    public void OpenChestAsset()
     {
-        Application.OpenURL("https://www.purple-planet.com/");
+        Application.OpenURL("https://assetstore.unity.com/packages/3d/props/interior/treasure-set-free-chest-72345");
     }
-
+    public void OpenPropPackAsset()
+    {
+        Application.OpenURL("https://assetstore.unity.com/packages/3d/props/industrial/prop-pack-30963");
+    }
+    public void OpenLobbyTimeMusic()
+    {
+        Application.OpenURL("https://incompetech.filmmusic.io/song/3986-lobby-time");
+    }
+    public void OpenCheeryMondayMusic()
+    {
+        Application.OpenURL("https://incompetech.filmmusic.io/song/3495-cheery-monday");
+    }
+    public void OpenCoinIcon()
+    {
+        Application.OpenURL("https://pixabay.com/vectors/dollar-money-icon-currency-object-3717534/");
+    }
+    public void OpenNoteIcon()
+    {
+        Application.OpenURL("https://www.bing.com/images/search?view=detailV2&ccid=TrK8UIh6&id=7D705DAD1004CBA1620DCBEC9D1F06CDED1F2D21&thid=OIP.TrK8UIh6y195zzY_oRC9HgHaHa&mediaurl=https%3a%2f%2fimage.flaticon.com%2ficons%2fpng%2f512%2f34%2f34732.png&exph=512&expw=512&q=note+icon&simid=608026145933102279&selectedIndex=56&qft=+filterui%3alicense-L2_L3_L4_L5_L6_L7&ajaxhist=0");
+    }
+    public void OpenKeyIcon()
+    {
+        Application.OpenURL("https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Octicons-key.svg/1200px-Octicons-key.svg.png");
+    }
+    public void OpenBoxIcon()
+    {
+        Application.OpenURL("https://www.bing.com/images/search?view=detailV2&ccid=tANdeslH&id=F6F14C8BC22B09AF222A410C15442560201718A5&thid=OIP.tANdeslH28G9942EF2fH3gHaGx&mediaurl=https%3a%2f%2fs3-us-west-2.amazonaws.com%2fcourses-images-archive-read-only%2fwp-content%2fuploads%2fsites%2f903%2f2016%2f01%2f23225801%2fblack-box-310220_1280.png&exph=1171&expw=1280&q=box+icon&simid=608012157317417034&selectedIndex=1&qft=+filterui%3alicense-L2_L3_L5_L6&ajaxhist=");
+    }
 }
